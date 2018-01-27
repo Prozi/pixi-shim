ad8e51f361e4eb7653f811f3c6a34b6e88c360f1
Jacek Pietal <prozi85@gmail.com>
2018-01-27 16:32:31 +0100

    added pixi dependency for including it ONCE for all inclusions'

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 
 # pixi.js backend shim 
 
-for using canvas in node with pixi.js and maybe tiled-utils
+for using canvas in node with pixi.js (and maybe tiled-utils)
+
+with lazy loading of
+
+* jsdom (window, ...)
+* Canvas
+* Image
+* PIXI (as singleton)
+
+returns PIXI instance
 
 https://github.com/Automattic/node-canvas/wiki/Installation---Windows
 
@@ -14,8 +23,7 @@ https://www.microsoft.com/en-us/download/details.aspx?id=48159
 example use (in node js env):
 ```
 # for pixi in node js
-require('pixi-shim')
-const PIXI = require('pixi.js')
+const PIXI = require('pixi-shim')
 
 # for tiled integration with pixi (in node/browser)
 const TiledUtils = require('tiled-utils')
--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-// pixi-shim.js
-require('jsdom-global')()
+'use strict'
 
-global.Canvas = require('canvas')
-global.Image = global.Canvas.Image
-
-// Node canvas Image's dont currently have `addEventListener` so we fake it for now.
-// We can always make updates to the node-canvas lib
-global.Image.prototype.addEventListener = function (event, fn) {
-  const img = this
-
-  switch (event) {
-    case 'error':
-      img.onerror = function () {
-        img.onerror = null
-        img.onload = null
-        fn.call(img)
-      }
-      break
-
-    case 'load':
-      img.onload = function () {
-        img.onerror = null
-        img.onload = null
-        fn.call(img)
-      }
-      break
-  }
+if (!global.window) {
+  require('jsdom-global')()  
 }
 
-global.Image.prototype.removeEventListener = function () {}
-global.navigator = { userAgent: 'node.js' } // could be anything
+if (!global.Canvas) {
+  global.Canvas = require('canvas')
+}
+
+if (!global.Image) {
+  global.Image = global.Canvas.Image
+
+  // Node canvas Image's dont currently have `addEventListener` so we fake it for now.
+  // We can always make updates to the node-canvas lib
+  global.Image.prototype.addEventListener = function (event, fn) {
+    const img = this
+
+    switch (event) {
+      case 'error':
+        img.onerror = function () {
+          img.onerror = null
+          img.onload = null
+          fn.call(img)
+        }
+        break
+
+      case 'load':
+        img.onload = function () {
+          img.onerror = null
+          img.onload = null
+          fn.call(img)
+        }
+        break
+    }
+  }
+
+  global.Image.prototype.removeEventListener = function () {}
+}
+
+if (!global.navigator) {
+  global.navigator = { userAgent: 'node.js' } // could be anything
+}
+
+if (!global.window.PIXI) {
+  global.window.PIXI = require('pixi.js')
+}
+
+module.exports = global.window.PIXI
+
+module.exports.default = module.exports
