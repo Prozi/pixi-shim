41967a126f269e46bc2484bc81299e4ac36db743
Jacek Pietal <prozi85@gmail.com>
2018-02-17 14:15:40 +0100

    [Feature] browser-env compatibility attempt + remove const for compatibility with older

--- index.js
+++ index.js
@@ -1,69 +1,83 @@
 'use strict'
 
-console.time('pixi-shim initialized in');
+console.time('pixi-shim: initialized in');
 
-// require once
-if (!global.window) {
-  console.log('pixi-shim polyfill dom')
-  require('jsdom-global')()  
-}
+if (typeof window === 'undefined') {
 
-// test for canvas capabilities
-const canvas = document.createElement('canvas')
-if (!canvas.getContext || !canvas.getContext('2d')) {
-  // requrie once
-  if (!global.Canvas) {
-    console.log('pixi-shim polyfill canvas for canvas')
-    global.Canvas = require('canvas-prebuilt')
+  // require once
+  if (!global.window) {
+    console.log('pixi-shim: polyfill dom')
+    require('jsdom-global')()  
   }
-}
 
-if (!global.Image) {
-  // this might be required for image
-  if (!global.Canvas) {
-    console.log('pixi-shim polyfill canvas for image')
-    global.Canvas = require('canvas-prebuilt')
-  }
-  global.Image = global.Canvas.Image
-
-  // Node canvas Image's dont currently have `addEventListener` so we fake it for now.
-  // We can always make updates to the node-canvas lib
-  global.Image.prototype.addEventListener = function (event, fn) {
-    const img = this
-
-    switch (event) {
-      case 'error':
-        img.onerror = function () {
-          img.onerror = null
-          img.onload = null
-          fn.call(img)
-        }
-        break
-
-      case 'load':
-        img.onload = function () {
-          img.onerror = null
-          img.onload = null
-          fn.call(img)
-        }
-        break
+  // test for canvas capabilities
+  var canvas = document.createElement('canvas')
+  if (!canvas.getContext || !canvas.getContext('2d')) {
+    // require once
+    if (!global.Canvas) {
+      console.log('pixi-shim: polyfill canvas for canvas')
+      global.Canvas = require('canvas-prebuilt')
     }
   }
 
-  global.Image.prototype.removeEventListener = function () {}
+  if (!global.Image) {
+    // this might be required for image
+    if (!global.Canvas) {
+      console.log('pixi-shim: polyfill canvas for image')
+      global.Canvas = require('canvas-prebuilt')
+    }
+    global.Image = global.Canvas.Image
+
+    // Node canvas Image's dont currently have `addEventListener` so we fake it for now.
+    // We can always make updates to the node-canvas lib
+    global.Image.prototype.addEventListener = function (event, fn) {
+      var img = this
+
+      switch (event) {
+        case 'error':
+          img.onerror = function () {
+            img.onerror = null
+            img.onload = null
+            fn.call(img)
+          }
+          break
+
+        case 'load':
+          img.onload = function () {
+            img.onerror = null
+            img.onload = null
+            fn.call(img)
+          }
+          break
+      }
+    }
+
+    global.Image.prototype.removeEventListener = function () {}
+  }
+
+  if (!global.navigator) {
+    global.navigator = { userAgent: 'node.js' } // could be anything
+  }
+
+  if (!global.window.PIXI) {
+    console.log('pixi-shim: load pixijs')
+    global.window.PIXI = require('pixi.js')
+  }
+
+  module.exports = global.window.PIXI
+} else {
+
+  console.warn('pixi-shim: detected browser environment')
+
+  // require once
+  if (!window.PIXI) {
+    console.log('pixi-shim: load pixijs')
+    window.PIXI = require('pixi.js')
+  }
+
+  module.exports = window.PIXI
 }
 
-if (!global.navigator) {
-  global.navigator = { userAgent: 'node.js' } // could be anything
-}
-
-if (!global.window.PIXI) {
-  console.log('pixi-shim load pixijs')
-  global.window.PIXI = require('pixi.js')
-}
-
-module.exports = global.window.PIXI
+console.timeEnd('pixi-shim: initialized in')
 
 module.exports.default = module.exports
-
-console.timeEnd('pixi-shim initialized in')
